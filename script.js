236b8c4ff1a40840265fbf7876432b5f2e2f81c6
sara_h <smoke_screen16@hotmail.com>
2021-11-02 23:13:19 -0400

    Included end time display

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer (seconds) {
    const now = Date.now();
    const then = now + seconds * 1000;
    //console.log({now,then});
    displayTimeLeft(seconds);
+   displayEndTime(then);
 
    countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -27,5 +29,12 @@ function displayTimeLeft(seconds) {
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
     document.title = display;
     timerDisplay.textContent = display;
+}
 
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textConent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
