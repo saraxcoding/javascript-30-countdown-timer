42dfe38dab63bde5a8d91a424bccbd6ed1d2b53d
sara_h <smoke_screen16@hotmail.com>
2021-11-02 23:23:15 -0400

    Changed margin formatting and included button functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll)[data-time]);
 
 function timer (seconds) {
+   // clear any existing timers
+   clearInterval(countdown);
+
    const now = Date.now();
    const then = now + seconds * 1000;
    //console.log({now,then});
@@ -10,31 +14,41 @@ function timer (seconds) {
    displayEndTime(then);
 
    countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop intervals
-    if (secondsLeft < 0) {
-        clearInterval(countdown);
-        return;
-    }
-    //display countdown
-    displayTimeLeft(secondsLeft);
+   const secondsLeft = Math.round((then - Date.now()) / 1000);
+   // check if we should stop intervals
+   if (secondsLeft < 0) {
+       clearInterval(countdown);
+       return;
+   }
+
+   //display countdown
+   displayTimeLeft(secondsLeft);
    }, 1000);
 }
 
 function displayTimeLeft(seconds) {
-    //console.log(seconds);
+   //console.log(seconds);
     
-    const minutes = Math.floor(seconds / 60);
-    const remainderSeconds = seconds % 60;
-    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
-    document.title = display;
-    timerDisplay.textContent = display;
+   const minutes = Math.floor(seconds / 60);
+   const remainderSeconds = seconds % 60;
+   const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+   document.title = display;
+   timerDisplay.textContent = display;
 }
 
 function displayEndTime(timestamp) {
-    const end = new Date(timestamp);
-    const hour = end.getHours();
-    const adjustedHour = hour > 12 ? hour - 12 : hour;
-    const minutes = end.getMinutes();
-    endTime.textConent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+   const end = new Date(timestamp);
+   const hour = end.getHours();
+   const adjustedHour = hour > 12 ? hour - 12 : hour;
+   const minutes = end.getMinutes();
+   endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+   //console.log(this.dataset.time);
+   const seconds = parseInt(this.dataset.time);
+   //console.log(seconds);
+   timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
